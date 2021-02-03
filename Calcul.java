dd854186ed4c75ba86d9945d0053432806345e56
Isabelle <1661159@etu.cegep-lanaudiere.qc.ca>
2021-02-03 10:25:08 -0500

    ajout methode multiplication

--- Calcul.java
+++ Calcul.java
@@ -7,4 +7,8 @@ public class Calcul {
     public static int soustraction(int n1, int n2){
         return n1 - n2;
     }
+
+    public static int mutliplication(int n1, int n2){
+        return n1 * n2;
+    }
 }
